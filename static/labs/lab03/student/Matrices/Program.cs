98f5bff0ec149b958553c514bbb1af0b31781a90
Tomasz Herman <tomasz.herman@protonmail.com>
2025-10-11 11:27:58 +0200

    Updated Program.cs in student version

--- static/labs/lab03/student/Matrices/Program.cs
+++ static/labs/lab03/student/Matrices/Program.cs
@@ -2,7 +2,6 @@
 // #define STAGE02
 // #define STAGE03
 // #define STAGE04
-// #define STAGE05
 
 
 using Matrices.Matrix;
@@ -18,36 +17,36 @@ static class Program
         Matrix dense = new DenseMatrix(4, 5);
         Matrix sparse = new SparseMatrix(4, 5);
         Console.WriteLine("Matrices should be zero initialized:");
-        Console.WriteLine($"Dense:\n{dense}");
-        Console.WriteLine($"Sparse:\n{sparse}");
+        Console.WriteLine($"Dense:\n{dense}\n");
+        Console.WriteLine($"Sparse:\n{sparse}\n");
 
         dense[1, 2] = 5;
         sparse[1, 2] = 5;
         Console.WriteLine("Matrices after changing value (1, 2) to 5:");
-        Console.WriteLine($"Dense:\n{dense}");
-        Console.WriteLine($"Sparse:\n{sparse}");
+        Console.WriteLine($"Dense:\n{dense}\n");
+        Console.WriteLine($"Sparse:\n{sparse}\n");
 #endif // STAGE01
 
 #if STAGE02
         Console.WriteLine("----- Stage 02 -----");
-        
-        Matrix identity = Matrix.Identity(5);
+
+        Matrix identity = new SparseMatrix(5, 5);
+        identity.Identity();
         Console.WriteLine($"Identity matrix:\n{identity}");
-        Console.WriteLine($"Identity matrix is a sparse matrix: {identity is SparseMatrix}");
         
         Random rnd = new Random(42);
         FillRandomly(dense, rnd);
         FillRandomly(sparse, rnd);
         Console.WriteLine("Random matrices:");
-        Console.WriteLine($"Dense:\n{dense}");
-        Console.WriteLine($"Sparse:\n{sparse}");
+        Console.WriteLine($"Dense:\n{dense}\n");
+        Console.WriteLine($"Sparse:\n{sparse}\n");
 
         dense = dense.Transpose();
         sparse = sparse.Transpose();
         
         Console.WriteLine("Transposed matrices:");
-        Console.WriteLine($"Dense:\n{dense}");
-        Console.WriteLine($"Sparse:\n{sparse}");
+        Console.WriteLine($"Dense:\n{dense}\n");
+        Console.WriteLine($"Sparse:\n{sparse}\n");
         
         Console.WriteLine($"Norm of a dense matrix: {dense.Norm()}");
         Console.WriteLine($"Norm of a sparse matrix: {sparse.Norm()}");
@@ -55,24 +54,6 @@ static class Program
         
 #if STAGE03
         Console.WriteLine("\n----- Stage 03 -----");
-        
-        dense = new DenseMatrix(2, 5);
-        sparse = new SparseMatrix(2, 5);
-        
-        dense[0, 2] = 5;
-        dense[1, 3] = 3;
-        dense[0, 4] = 2;
-        
-        sparse[0, 2] = 5;
-        sparse[1, 3] = 3;
-        sparse[0, 4] = 2;
-        
-        Console.WriteLine($"Fill rate of dense matrix: {dense.FillRate()}");
-        Console.WriteLine($"Fill rate of sparse matrix: {sparse.FillRate()}");
-#endif // STAGE03
-        
-#if STAGE04
-        Console.WriteLine("\n----- Stage 04 -----");
         
         Matrix m1 = new DenseMatrix(3, 5);
         Matrix m2 = new SparseMatrix(3, 5);
@@ -83,37 +64,47 @@ static class Program
         FillRandomly(m3, rnd);
         
         Console.WriteLine("Random matrices:");
-        Console.WriteLine($"m1:\n{m1}");
-        Console.WriteLine($"m2:\n{m2}");
-        Console.WriteLine($"m3:\n{m3}");
+        Console.WriteLine($"m1:\n{m1}\n");
+        Console.WriteLine($"m2:\n{m2}\n");
+        Console.WriteLine($"m3:\n{m3}\n");
 
         Matrix addition = m1 + m2;
-        Matrix subtraction = m1 - m2;
+        Matrix subtraction = m2 - m1;
         Matrix scalarMul = 2.0f * m1;
-        Matrix matrixMul = m1 * m3;
+        Matrix matrixMul = m2 * m3;
         
         Console.WriteLine($"m1, m2 addition result:\n{addition}");
-        Console.WriteLine($"m1, m2 subtraction result:\n{subtraction}");
-        Console.WriteLine($"m1 and 2.0f multiplication result:\n{scalarMul}");
-        Console.WriteLine($"m1, m3 multiplication result:\n{matrixMul}");
-#endif // STAGE04
+        Console.WriteLine($"Addition result type: {addition.GetType()}\n");
         
-#if STAGE05
+        Console.WriteLine($"m2, m1 subtraction result:\n{subtraction}");
+        Console.WriteLine($"Subtraction result type: {subtraction.GetType()}\n");
+        
+        Console.WriteLine($"m1 and 2.0f multiplication result:\n{scalarMul}");
+        Console.WriteLine($"Multiplication with float result type: {scalarMul.GetType()}\n");
+        
+        Console.WriteLine($"m2, m3 multiplication result:\n{matrixMul}");
+        Console.WriteLine($"Multiplication result type: {matrixMul.GetType()}\n");
+#endif // STAGE03
+
+#if STAGE04
+        Console.WriteLine("\n----- Stage 04 -----");
+
         // This matrix is reversible
         Matrix m = new  DenseMatrix(3, 3);
         m[0, 0] = 5;    m[0, 1] = 4;    m[0, 2] = 2;
         m[1, 0] = 9;    m[1, 1] = 5;    m[1, 2] = 4;
         m[2, 0] = 0;    m[2, 1] = 8;    m[2, 2] = 9;
         
-        Console.WriteLine($"Matrix m:\n{m}");
+        Console.WriteLine($"Matrix m:\n{m}\n");
 
         Matrix pseudoInv = MatrixAlgorithms.PseudoInverse(m);
         
-        Console.WriteLine($"pseudoInvM:\n{pseudoInv}");
-        Console.WriteLine($"m*pseudoInvM:\n{m*pseudoInv}");
-#endif // STAGE05
+        Console.WriteLine($"pseudoInvM:\n{pseudoInv}\n");
+        Console.WriteLine($"m*pseudoInvM:\n{m*pseudoInv}\n");
+#endif // STAGE04
     }
 
+    
     static void FillRandomly(Matrix matrix, Random generator, int minVal = 0, int maxVal = 10)
     {
         for (int row = 0; row < matrix.Rows; row++)
