29d6c889e474418b81c123b247fc2d2e3f4b78f5
Lumek4 <lumek4@gmail.com>
2026-01-23 11:43:15 +0100

    Workshop 8

---
+++ static/labs/lab14/student/Binding/PatternGeneration/NativeMethods.cs
@@ -0,0 +1,9 @@
+using System.Drawing;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+using PatternGeneration;
+
+internal static partial class NativeMethods
+{
+    private const string LibName = "pg";
+}
---
+++ static/labs/lab14/student/SafeHandles/FileInteraction/WindowsFile.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileInteraction
+{
+    internal partial class MyFile
+    {
+        private MyFile(PlatformID platformID)
+        {
+            if (platformID != PlatformID.Win32NT)
+                throw new PlatformNotSupportedException();
+        }
+
+        [SuppressUnmanagedCodeSecurity()]
+        private partial class NativeFile
+        {
+            [LibraryImport("kernel32", EntryPoint = "CreateFileA", SetLastError = true, StringMarshalling = StringMarshalling.Utf8)]
+            internal static partial nint CreateFile(string fileName,
+               System.IO.FileAccess dwDesiredAccess, System.IO.FileShare dwShareMode,
+               IntPtr securityAttrs_MustBeZero, System.IO.FileMode dwCreationDisposition,
+               int dwFlagsAndAttributes, IntPtr hTemplateFile_MustBeZero);
+            [LibraryImport("kernel32", SetLastError = true)]
+            internal static unsafe partial int ReadFile(/*MyFile*/ IntPtr handle, byte* bytes,
+           int numBytesToRead, out int numBytesRead, IntPtr overlapped_MustBeZero);
+            [return: MarshalAs(UnmanagedType.Bool)]
+            [LibraryImport("kernel32", SetLastError = true)]
+            internal static partial bool CloseHandle(/*MyFile*/ IntPtr handle);
+        }
+    }
+}
